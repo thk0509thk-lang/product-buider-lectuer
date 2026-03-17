953a9c87fa6192a5b634e5a6ea7dd361d28c5924
thk0509thk <thk0509thk@gmail.com>
2026-03-17 09:31:59 +0000

    feat: create lotto number generator

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const themeToggle = document.getElementById('theme-toggle');
+const generatorBtn = document.getElementById('generator-btn');
+const lottoNumbersDiv = document.getElementById('lotto-numbers');
 
-themeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
+generatorBtn.addEventListener('click', () => {
+  const numbers = new Set();
+  while (numbers.size < 6) {
+    numbers.add(Math.floor(Math.random() * 45) + 1);
+  }
+
+  lottoNumbersDiv.textContent = Array.from(numbers).join(', ');
 });
